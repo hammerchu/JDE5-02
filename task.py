aa784a4bf451012c2804a7e3800ea4dead6c856e
Jenny Wong <121168536+jennywong2023@users.noreply.github.com>
2022-12-24 01:21:52 +0800

    Updated Task 2  V2 - punctuation
    Updated Task 2  V2 - punctuation

--- task.py
+++ task.py
@@ -37,6 +37,9 @@ if __name__ == "__main__":
     # print(taskThree())
     # print(taskFour())
 
+    
+    
+    
     # Task-1 - count the total number of words in the prargraph that contains vowel characters(a, e, i, o u)
     
     # Task-2 - encode the paragraph by shifting the position of each character by a variable value e.g. I am a boy (1) -> J bn b cpz
